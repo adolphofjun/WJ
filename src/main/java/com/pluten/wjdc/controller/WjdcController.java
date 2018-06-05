5121f2d49c3746bc8fcef32e06189aa31863199f
wangxiangjun <adolph_jun@163.com>
2018-06-06 00:25:15 +0800

    excel导入

--- src/main/java/com/pluten/wjdc/controller/WjdcController.java
+++ src/main/java/com/pluten/wjdc/controller/WjdcController.java
@@ -1,6 +1,7 @@
 package com.pluten.wjdc.controller;
 
 import com.alibaba.fastjson.JSON;
+import com.pluten.utils.ReadExcel;
 import com.pluten.utils.ResultMsg;
 import com.pluten.utils.ResultUtil;
 import com.pluten.wjdc.service.WjdcService;
@@ -8,11 +9,12 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -47,22 +49,72 @@ public class WjdcController {
         return resultMsg;
     }
 
+   /* @RequestMapping(value = "importExcel",method = RequestMethod.POST)
+    @ApiOperation(value = "新建规则", notes = "新建规则")
+    @ResponseBody
+    public ResultMsg importExcel(@RequestBody @ApiParam(name = "角色", value = "传入json格式{\n" +
+            "\t\"name\": \"规则1\",\n" +
+            "\t\"code\": \"入了1\",\n" +
+            "\t\"belong_role\": \"1\",\n" +
+            "\t\"must_bank\": [\"1\", \"2\"],\n" +
+            "\t\"random_bank\": [\"1\", \"2\"],\n" +
+            "\t\"visibility\": \"0\"\n" +
+            "}", required = true) Map rule){
+        ResultMsg resultMsg;
+        try {
+            wjdcService.saveRule(rule);
+            resultMsg = ResultUtil.success("新建规则成功",rule);
+        } catch (Exception e) {
+            e.printStackTrace();
+            resultMsg = ResultUtil.systemError();
+        }
+        return resultMsg;
+    }*/
 
-    public static void main(String[] str){
-        Map map = new HashMap();
-        List<String> must = new ArrayList<String>();
-        must.add("1");
-        must.add("2");
-        List<String> random = new ArrayList<String>();
-        random.add("1");
-        random.add("2");
-        map.put("name","规则一");
-        map.put("code","rule1");
-        map.put("visibility","1");
-        map.put("must_bank",must);
-        map.put("random_bank",random);
-        System.out.println("==="+map.toString());
+
+    @RequestMapping(value = "importExcel", method = RequestMethod.POST)
+    @ApiOperation(value = "excel导入", notes = "excel导入")
+    @ResponseBody
+    public String batchimport(MultipartFile file,
+                              HttpServletRequest request, HttpServletResponse response) throws IOException {
+        //log.info("AddController ..batchimport() start");
+        //判断文件是否为空
+        if(file==null) return null;
+        //获取文件名
+        String name=file.getOriginalFilename();
+        System.out.println("========文件名"+name);
+        //进一步判断文件是否为空（即判断其大小是否为0或其名称是否为null）
+        long size=file.getSize();
+        if(name==null || ("").equals(name) && size==0) return null;
+
+        //批量导入。参数：文件名，文件。
+        boolean b = batchImport(name,file);
+        if(b){
+            String Msg ="批量导入EXCEL成功！";
+            request.getSession().setAttribute("msg",Msg);
+        }else{
+            String Msg ="批量导入EXCEL失败！";
+            request.getSession().setAttribute("msg",Msg);
+        }
+        return "Customer/addCustomer3";
     }
+
+    public boolean batchImport(String name,MultipartFile file){
+        boolean b = false;
+        //创建处理EXCEL
+        ReadExcel readExcel=new ReadExcel();
+        //解析excel，获取客户信息集合。
+         readExcel.getExcelInfo(name ,file);
+
+
+
+        //迭代添加客户信息（注：实际上这里也可以直接将customerList集合作为参数，在Mybatis的相应映射文件中使用foreach标签进行批量添加。）
+
+        return b;
+    }
+
+
+
 
 }
 
