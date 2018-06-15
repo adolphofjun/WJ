350299e1e98a51afd8f62cf5ee6968ec4b0d2c65
wangxiangjun <adolph_jun@163.com>
2018-06-15 16:08:07 +0800

    访问跨域问题

--- src/main/java/com/pluten/filter/EncoidingFilter.java
+++ src/main/java/com/pluten/filter/EncoidingFilter.java
@@ -1,4 +1,25 @@
 package com.pluten.filter;
 
-public class EncoidingFilter {
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class EncoidingFilter implements Filter {
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+        System.out.println("过撸起=============");
+        chain.doFilter(req, res);
+    }
+
+    public void destroy() {
+
+    }
 }
